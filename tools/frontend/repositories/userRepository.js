b3d59c71ad00ea9e6fba6757adffe204de0dc2c5
kerokerogeorge <kerokerogeorge@yahoo.co.jp>
2022-09-08 17:01:04 +0900

    APIの呼び出し準備

--- tools/frontend/repositories/userRepository.js
+++ tools/frontend/repositories/userRepository.js
@@ -3,5 +3,39 @@ import repository from './repository'
 export default {
   getUsers () {
     return repository.get('/user/list')
+  },
+  getUser (token) {
+    return repository.get('/user', {
+      headers: {
+        'x-token': `${token}`
+      }
+    })
+  },
+  createUser (name) {
+    return repository.post('/user', {
+      name
+    })
+  },
+  getUserCharacters (token) {
+    return repository.get('/user/characters', {
+      headers: {
+        'x-token': `${token}`
+      }
+    })
+  },
+  update (token, params) {
+    return repository.put('/user', {
+      headers: {
+        'x-token': `${token}`
+      }
+    },
+    { params })
+  },
+  delete (token) {
+    return repository.delete('/user', {
+      headers: {
+        'x-token': `${token}`
+      }
+    })
   }
 }
