b61090f17fcae11d394016fe240b02e21cdd4f7d
kerokerogeorge <kerokerogeorge@yahoo.co.jp>
2022-09-14 18:44:10 +0900

    metamaskに接続、TransactionをListen

--- tools/frontend/layouts/default.vue
+++ tools/frontend/layouts/default.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="w-screen">
     <div class="flex items-center pl-56 h-28 fixed z-20 bg-white border-b border-solid border-gray-400 w-full">
-      <div class="text-2xl font-bold">API動作確認</div>
-      <div class="ml-auto">
-        <button @click="login">Login</button>
-        <div>
+      <div>
+        <div class="text-2xl font-bold">API動作確認</div>
+        <div class="text-xs text-gray-600">
+          <div>
           アドレス: {{ address ? address : '-' }}
+          </div>
+          <div>
+            ETH: {{ etherBalance ? etherBalance : '-' }}
+          </div>
+          <div>
+            {{ symbol ? symbol : '-' }}トークン: {{ balance ? balance : '-' }}
+          </div>
         </div>
-        <div>
-          {{ symbol ? symbol : '-' }}: {{ balance ? balance : '-' }}
-        </div>
+      </div>
+      <div v-if="!isConnected" class="ml-auto w-auto pr-5">
+        <button class="bg-gray-400 hover:bg-gray-700 text-xs cursor-pointer rounded-sm p-1 text-white" @click="connectToMetamask">Metamaskに接続</button>
       </div>
     </div>
     <div class="relative">
@@ -26,20 +33,28 @@
 
 <script>
 import { ethers } from "ethers";
-import ABI_JSON from "../static/abi.json"
+import { mapGetters, mapActions } from 'vuex'
+// import ABI_JSON from "../static/abi.json"
+import CONTRACT_ABI_JSON from "../static/contractAbi.json"
 
 const tokenContractAddress = '0x984a6eaecBE9e77339931E191B6bf314c6f65dab'
 
 export default {
   data() {
     return {
+      abi: CONTRACT_ABI_JSON,
       provider: null,
       accounts: [],
       balance: null,
       address: null,
-      abi: ABI_JSON,
-      symbol: null
+      symbol: null,
+      etherBalance: null
     }
+  },
+  computed: {
+    ...mapGetters('token', [
+      'isConnected'
+    ]),
   },
   async mounted () {
     try {
@@ -52,21 +67,39 @@ export default {
     }
   },
   methods: {
-    async login () {
+    ...mapActions('token', [
+      'changeConnectionStatus'
+    ]),
+    async connectToMetamask () {
       try {
         if(!window.ethereum) return
+        this.accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         provider.getBalance("0x63c7a33d940113c8d9634fff125efa564aa4cc0c").then((result)=>{
-          this.balance = ethers.utils.formatEther(result)
+          this.etherBalance = ethers.utils.formatEther(result)
         })
-
-        this.accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
         const signer = await provider.getSigner()
         this.address = await signer.getAddress()
         const contract = new ethers.Contract(tokenContractAddress, this.abi, provider)
         const result = (await contract.balanceOf(this.address)).toString()
         this.symbol = await contract.symbol()
         this.balance = await ethers.utils.formatUnits(result)
+
+        await contract.on("Transfer", async (from, to, value, event) => {
+          let info = {
+            from: from,
+            to: to,
+            value: ethers.utils.formatUnits(value),
+            data: event,
+          };
+          console.log(JSON.stringify(info, null, 4));
+          console.log('=======================')
+          console.log(info.value)
+          console.log('=======================')
+          const b = (await contract.balanceOf(this.address)).toString()
+          this.balance = ethers.utils.formatUnits(b)
+        })
+        await this.changeConnectionStatus({ isConnected: true })
       } catch (e) {
         console.log(e)
       }
